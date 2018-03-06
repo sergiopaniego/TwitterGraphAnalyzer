5e0caf254dd3bed8f01db81b1fe292e85df649ef
sergiopaniego <sergiopaniegoblanco@gmail.com>
2018-03-06 23:22:26 +0100

    FormView and ListView added to graph interface

--- graphanalyzer/urls.py
+++ graphanalyzer/urls.py
@@ -5,5 +5,5 @@ from . import views
 app_name = 'graphanalyzer'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('graph', views.graph, name='graph')
+    path('graph', views.GraphView.as_view(success_url="graph"), name='graph')
 ]
--- graphanalyzer/views.py
+++ graphanalyzer/views.py
@@ -1,14 +1,50 @@
+import threading
 from django.shortcuts import render
+from django.views.generic import ListView
+from py2neo import Graph
+import secrets
 from .forms import HashtagForm
 import graphanalyzer.twitterManager as twitterManager
+from django.views.generic.edit import FormView
 
 
 def index(request):
     return render(request, 'graphanalyzer/index.html')
 
-def graph(request):
-    if request.method == 'POST':
-        form = HashtagForm(request.POST)
-        if form.is_valid():
-            twitterManager.connectToStream(form.cleaned_data['hashtag'])
-    return render(request, 'graphanalyzer/graph.html')
+class GraphView(FormView, ListView):
+    template_name = 'graphanalyzer/graph.html'
+    context_object_name = 'graph'
+    form_class = HashtagForm
+
+    def form_valid(self, form):
+        # This method is called when valid form data has been POSTed.
+        # It should return an HttpResponse.
+        thread1 = myThread(1, "Thread-1", form)
+        thread1.start()
+        return super().form_valid(form)
+
+    def get_queryset(self):
+        graph = Graph(password=secrets.password)
+        return graph.data("MATCH (a:Person) RETURN a.name, a.born LIMIT 4")
+
+    '''
+    def graph(self, **kwargs):
+        if request.method == 'POST':
+            form = HashtagForm(request.POST)
+            if form.is_valid():
+                thread1 = myThread(1, "Thread-1", form)
+                thread1.start()
+        return render(request, 'graphanalyzer/graph.html')
+    '''
+
+class myThread(threading.Thread):
+    def __init__(self, threadID, name, form):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+        self.form = form
+
+    def run(self):
+        print("Starting " + self.name)
+        twitterManager.connectToStream(self.form.cleaned_data['hashtag'])
+        print("Exiting " + self.name)
